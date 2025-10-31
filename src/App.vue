90fbfad79af73fd2c3c4f9ef8bb5b287ba8dd911
neajcoffee <neajblvck@icloud.com>
2025-10-31 22:14:26 +0800

    maj

--- src/App.vue
+++ src/App.vue
@@ -8,28 +8,17 @@
     <div v-if="notification" class="notification" :class="notification.type">
       {{ notification.message }}
     </div>
-
-    <!-- <LoadingOverlay /> -->
-    <LoadingOverlay v-if="showLoadingOverlay" />
   </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
 import AppLayout from './components/layout/AppLayout.vue'
-import LoadingOverlay from './components/base/LoadingOverlay.vue'
 
 export default {
   name: 'App',
   components: {
-    AppLayout,
-    LoadingOverlay
-  },
-  data() {
-    return {
-      isAppLoaded: false,
-      isAuthReady: false
-    }
+    AppLayout
   },
   
   computed: {
@@ -38,51 +27,16 @@ export default {
     
     isLandingPage() {
       return this.$route.name === 'LandingPage'
-    },
-    showLoadingOverlay() {
-      return !(this.isAppLoaded && this.isAuthReady)
     }
   },
   
   async created() {
     // Vérifier le token au démarrage de l'application
-    try {
-      await this.initializeAuth()
-    } finally {
-      this.isAuthReady = true
-    }
-  },
-  mounted() {
-    if (typeof window === 'undefined') {
-      this.isAppLoaded = true
-      return
-    }
-
-    if (document.readyState === 'complete') {
-      this.handleAppLoaded()
-    } else {
-      window.addEventListener('load', this.handleAppLoaded)
-    }
-  },
-  beforeUnmount() {
-    this.cleanupLoadListener()
-  },
-  // Vue 2 fallback
-  beforeDestroy() {
-    this.cleanupLoadListener()
+    await this.initializeAuth()
   },
   
   methods: {
-    ...mapActions('auth', ['initializeAuth']),
-    handleAppLoaded() {
-      this.isAppLoaded = true
-      this.cleanupLoadListener()
-    },
-    cleanupLoadListener() {
-      if (typeof window !== 'undefined') {
-        window.removeEventListener('load', this.handleAppLoaded)
-      }
-    }
+    ...mapActions('auth', ['initializeAuth'])
   }
 }
 </script>
